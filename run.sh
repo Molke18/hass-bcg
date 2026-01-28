163545fa1bf1931760053c343bd32afb4b23694e
Molke18 <36983482+Molke18@users.noreply.github.com>
2026-01-28 10:00:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,8 +7,11 @@ MQTT_HOST=$(bashio::config 'mqtt_host')
 MQTT_HOST_PORT=$(bashio::config 'mqtt_host_port')
 USB_PORT=$(bashio::config 'usb_port')
 DEBUG=$(bashio::config 'debug')
+CONFIG_FILE=$(bashio::config 'config_file')
+DATA_UNIT=$(<$CONFIG_FILE)
 
 bashio::log.info "Running bcg on ${USB_PORT}. Connecting to ${MQTT_HOST} on port ${MQTT_HOST_PORT} using username ${MQTT_USERNAME} ..."
+bashio::log.info "Test ${DATA_UNIT}" 
 
 if [ "$DEBUG" = true ]
  then 
