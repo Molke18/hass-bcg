126ef77d089ebdd66f957040d0c480654e5477d9
Frederick Molke <36983482+Molke18@users.noreply.github.com>
2026-01-30 19:13:15 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -e
 CONFIG_FILE=$(bashio::config 'config_file')
 DATA_UNIT=$(<$CONFIG_FILE)
 bashio::log.info "Reading configuration from  ${CONFIG_FILE}"
-bashio::log.info "Test ${DATA_UNIT}" 
-#bcg -c ${CONFIG_FILE}
+#bashio::log.info "Test ${DATA_UNIT}" 
+bcg -c ${CONFIG_FILE}
