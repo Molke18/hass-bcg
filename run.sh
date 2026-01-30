c01fcad43127e5a59ebc0ce4c27fd92d5ce04952
Frederick Molke <36983482+Molke18@users.noreply.github.com>
2026-01-30 13:51:08 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,5 @@
 #!/usr/bin/with-contenv bashio
 set -e
-
-MQTT_USERNAME=$(bashio::config 'mqtt_username')
-MQTT_PASSWORD=$(bashio::config 'mqtt_password')
-MQTT_HOST=$(bashio::config 'mqtt_host')
-MQTT_HOST_PORT=$(bashio::config 'mqtt_host_port')
-USB_PORT=$(bashio::config 'usb_port')
-DEBUG=$(bashio::config 'debug')
 CONFIG_FILE=$(bashio::config 'config_file')
-DATA_UNIT=$(<$CONFIG_FILE)
-
-bashio::log.info "Running bcg on ${USB_PORT}. Connecting to ${MQTT_HOST} on port ${MQTT_HOST_PORT} using username ${MQTT_USERNAME} ..."
-bashio::log.info "Test ${DATA_UNIT}" 
-
-if [ "$DEBUG" = true ]
- then 
-  bashio::log.info "DEBUG is ON"
-  bcg -H ${MQTT_HOST} -P ${MQTT_HOST_PORT} --mqtt-username ${MQTT_USERNAME} --mqtt-password ${MQTT_PASSWORD} -d ${USB_PORT} --debug
- else
-  bashio::log.info "DEBUG is OFF"
-  bcg -H ${MQTT_HOST} -P ${MQTT_HOST_PORT} --mqtt-username ${MQTT_USERNAME} --mqtt-password ${MQTT_PASSWORD} -d ${USB_PORT}
-fi
+bashio::log.info "Reading configuration from  ${CONFIG_FILE}"
+bcg -c ${CONFIG_FILE}
